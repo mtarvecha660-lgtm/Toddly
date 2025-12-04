f08fcf9e123702b5f46d52d6a72c2e3d07c36b0f
RehanKahtri <rehankhatri502@gmail.com>
2025-12-04 11:25:50 +0530

    Initial Commit

---
+++ Toddly/ToddlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToddlyApp.swift
+//  Toddly
+//
+//  Created by student on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToddlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
