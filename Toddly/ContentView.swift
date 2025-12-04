fdb8bf757d3e6f4914498bfb29476ae74d74e077
RehanKahtri <rehankhatri502@gmail.com>
2025-12-04 12:52:34 +0530

    begin

--- Toddly/ContentView.swift
+++ Toddly/ContentView.swift
@@ -6,11 +6,41 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext)
+        private var modelContext
+    
+    @Query private var lists: [Listt]
+    
     var body: some View {
-        VStack {
-            
+        NavigationStack {
+            List{
+                ForEach(lists)
+                {
+                    list in
+                    Text(list.title)
+                }
+            }
+            .navigationTitle("MyList")
+            .toolbar{
+                ToolbarItem(placement:.topBarTrailing){
+                    Button{
+                        
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    
+                }
+            }
+            .overlay{
+                if lists.isEmpty{
+                    ContentUnavailableView("Add your item",
+                                           systemImage:"heart.circle",description: Text("No item is added. Add one"))
+                }
+            }
         }
     }
 }
---
+++ Toddly/ListModel.swift
@@ -0,0 +1,19 @@
+//
+//  ListModel.swift
+//  Toddly
+//
+//  Created by student on 04/12/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Listt {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
