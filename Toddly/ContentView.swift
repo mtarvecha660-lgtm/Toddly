645c513fd8345c3b5a1c30862d668f36710c2587
RehanKahtri <rehankhatri502@gmail.com>
2025-12-04 15:29:29 +0530

    complete

--- Toddly/ContentView.swift
+++ Toddly/ContentView.swift
@@ -11,9 +11,12 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext)
-        private var modelContext
+    private var modelContext
     
     @Query private var lists: [Listt]
+    
+    @State private var title: String = ""
+    @State private var isAlertShowing: Bool = false
     
     var body: some View {
         NavigationStack {
@@ -22,18 +25,37 @@ struct ContentView: View {
                 {
                     list in
                     Text(list.title)
+                        .font(.title2)
+                        .fontWeight(.light)
+                        .padding(.vertical,2)
+                        .swipeActions{
+                            Button("Delete", role: .destructive){
+                                modelContext.delete(list)
+                            }
+                        }
                 }
             }
             .navigationTitle("MyList")
             .toolbar{
                 ToolbarItem(placement:.topBarTrailing){
                     Button{
-                        
+                        isAlertShowing.toggle()
                     } label: {
                         Image(systemName: "plus")
+                            .imageScale(.large)
                     }
-                    
                 }
+            }
+            .alert("Create a new item", isPresented: $isAlertShowing){
+                TextField("Enter a item", text: $title)
+                
+                Button(){
+                    modelContext.insert(Listt(title: title))
+                    title = ""
+                }label:{
+                    Text("Save")
+                }
+                .disabled(title.isEmpty)
             }
             .overlay{
                 if lists.isEmpty{
@@ -45,6 +67,19 @@ struct ContentView: View {
     }
 }
 
-#Preview {
+#Preview("Second List") {
+    let container = try! ModelContainer(for: Listt.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    let ctx = container.mainContext
+    ctx.insert(Listt(title: "Swift Coding Club"))
+    ctx.insert(Listt(title: "Laundary"))
+    ctx.insert(Listt(title: "Homework"))
+    
+    return ContentView()
+        .modelContainer(container)
+}
+
+#Preview("Main List") {
     ContentView()
+        .modelContainer(for: Listt.self, inMemory: true)
 }
